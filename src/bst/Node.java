72cc29e6051c86f6ecdd4e43fe714e8f4d7091ba
MounikaAlloju <34786164+ALLOJU@users.noreply.github.com>
2022-03-06 17:56:11 +0530

    Update Node.java

--- src/bst/Node.java
+++ src/bst/Node.java
@@ -5,15 +5,15 @@ package bst;
  *
  *node class that defines BST node
  */
-public class Node {
-	int key;
-	Node left;
-	Node right;
+public class Node<K extends Comparable<K>> {
+	K key;
+	Node<K> left;
+	Node<K>right;
 	/**
 	 * creating constructor for the node class
 	 * @param data - taking input as data
 	 */
-	public Node(int data) {
+	public Node(K data) {
 		
 		key = data;
 		left=right=null;
